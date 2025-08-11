e83a7395bf7f64f6b92785ebeec498ff800b5297
郭彦琪 <guoyanqi@jiudaotech.com>
2025-08-11 09:49:33 +0800

    更新博客配置，修改欢迎语，添加“本站大部分文章由 AI 润色输出，请注意甄别”的说明。

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,Hi，我是超级摸鱼王,欢迎来到我的博客🎉,本站大部分文章由 AI 润色输出，请注意甄别',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
